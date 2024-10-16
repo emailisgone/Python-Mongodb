ab78fd0fae1c2cbf55c6a147906bcca4de96af80
emailisgone <nikita.exe0@gmail.com>
2024-10-17 00:22:47 +0300

    New features and other fixes

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ client = MongoClient('localhost', 27017)
 
 db = client.flaskDatabase
 clientsCollection = db.clients
+productsCollection = db.products
 
 clientCounter = 0
+orderCounter = 0
+totalValueCounter = 0
 
 @app.route('/clients', methods=['PUT'])
 def registerClient():
@@ -59,6 +62,62 @@ def deleteClient(clientId):
         return 'Client not found', 404
 
     return 'Client deleted', 204
+
+@app.route('/products', methods=['PUT'])
+def registerProduct():
+    data = request.get_json()
+
+    if not data or 'id' not in data or 'name' not in data or 'price' not in data or data['price']<0:
+        return 'Invalid input, missing name or price', 400
+
+    existingProduct = productsCollection.find_one({'id': data['id']})
+    if existingProduct:
+        return 'Product with this id already exists', 400
+
+    newProduct = {
+        'id': data['id'],
+        'name': data['name'],
+        'category': data['category'],
+        'description': data['description'],
+        'price': data['price']
+    }
+    productsCollection.insert_one(newProduct)
+
+    return jsonify({'id': newProduct['id']}), 201
+
+@app.route('/products', methods=['GET'])
+def listProducts():
+    try:
+        data = request.get_json()
+    except Exception as e:
+        data = None
+
+    if not data or 'category' not in data:
+        productList = list(productsCollection.find({}, {'_id': 0}))
+        return jsonify(productList), 200
+
+    productList = list(productsCollection.find({'category': data['category']}, {'_id': 0}))
+    return jsonify(productList), 200
+
+@app.route('/products/<productId>', methods=['GET'])
+def getProductDetails(productId):
+    product = productsCollection.find_one({'id': productId}, {'_id': 0})
+
+    if not product:
+        return 'Product not found', 404
+
+    return jsonify(product), 200
+
+@app.route('/products/<productId>', methods=['DELETE'])
+def deleteProduct(productId):
+    product = productsCollection.find_one({'id': productId}, {'_id': 0})
+
+    if not product:
+        return 'Product not found', 404
+
+    productsCollection.delete_one({'id': productId})
+
+    return 'Product deleted', 204
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
