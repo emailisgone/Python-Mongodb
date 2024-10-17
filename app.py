c2e623b677a14219d446aaff0bfcf7b79b895f1f
emailisgone <nikita.exe0@gmail.com>
2024-10-17 16:55:05 +0300

    Finished with the rest of the requirements

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ client = MongoClient('localhost', 27017)
 db = client.flaskDatabase
 clientsCollection = db.clients
 productsCollection = db.products
+ordersCollection = db.orders
 
 clientCounter = 0
+orderCounter = 0
 
 @app.route('/clients', methods=['PUT'])
 def registerClient():
     global clientCounter
+    clientCounter = clientsCollection.count_documents({})
     
     data = request.get_json()
 
@@ -116,6 +119,149 @@ def deleteProduct(productId):
     productsCollection.delete_one({'id': productId})
 
     return 'Product deleted', 204
+
+@app.route('/orders', methods=['PUT'])
+def createOrder():
+    global orderCounter
+    orderCounter = ordersCollection.count_documents({})
+
+    data = request.get_json()
+
+    if not data or 'items' not in data or 'clientId' not in data:
+        return 'Invalid input, missing clientId or items', 400
+
+    if not clientsCollection.find_one({'id': data['clientId']}):
+        return 'Client not found', 404
+
+    for product in data['items']:
+        if not productsCollection.find_one({'id': product['productId']}):
+            return 'Product not found', 404
+        
+        if product['quantity'] < 1:
+            return 'Invalid quantity', 400
+
+    newOrder = {
+        'id': f"ord{orderCounter+1}",
+        'clientId': data['clientId'],
+        'items': data['items']
+    }
+    orderCounter += 1
+    ordersCollection.insert_one(newOrder)
+
+    return jsonify({'id': newOrder['id']}), 200
+
+@app.route('/clients/<clientId>/orders', methods=['GET'])
+def getClientOrders(clientId):
+    if not clientsCollection.find_one({'id': clientId}):
+        return 'Client not found', 404
+
+    orderList = list(ordersCollection.find({'clientId': clientId}, {'_id': 0}))
+    for order in orderList:
+        order.pop('clientId', None)
+
+    return jsonify(orderList), 200
+
+@app.route('/statistics/top/clients', methods=['GET'])
+def getTopTenClients():
+    clientList = list(ordersCollection.aggregate([
+        {
+            "$group": {
+                "_id": "$clientId",  
+                "totalOrders": { "$sum": 1 }  
+            }
+        },
+        { 
+            "$sort": { "totalOrders": -1 }  
+        },
+        { 
+            "$limit": 10  
+        },
+        {
+            "$project": {
+                "_id": 0,  
+                "clientId": "$_id",  
+                "totalOrders": 1  
+            }
+        }
+    ]))
+
+
+    return jsonify(clientList), 200
+
+@app.route('/statistics/top/products', methods=['GET'])
+def getTopTenProducts():
+    productList = list(ordersCollection.aggregate([
+        {
+            "$unwind": "$items" 
+        },
+        {
+            "$group": {
+                "_id": "$items.productId", 
+                "totalQuantity": { "$sum": "$items.quantity" }
+            }
+        },
+        {
+            "$sort": { "totalQuantity": -1 } 
+        },
+        {
+            "$limit": 10 
+        },
+        {
+            "$project": {
+                "_id": 0,  
+                "productId": "$_id",  
+                "totalQuantity": 1 
+            }
+        }
+    ]))
+
+    return jsonify(productList), 200
+
+@app.route('/statistics/orders/total', methods=['GET'])
+def getTotalOrdersNumber():
+    return jsonify({'total': ordersCollection.count_documents({})}), 200
+
+@app.route('/statistics/orders/totalValue', methods=['GET'])
+def getTotalValueOfOrders():
+    totalValue = list(ordersCollection.aggregate([
+        {
+            "$unwind": "$items"  
+        },
+        {
+            "$lookup": { 
+                "from": "products",
+                "localField": "items.productId",
+                "foreignField": "id",
+                "as": "productDetails"
+            }
+        },
+        {
+            "$unwind": "$productDetails"
+        },
+        {
+            "$group": {
+                "_id": None,
+                "totalValue": {
+                    "$sum": { "$multiply": ["$items.quantity", "$productDetails.price"] }
+                }  
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,  
+                "totalValue": 1 
+            }
+        }
+    ]))
+
+    return jsonify(totalValue[0]), 200
+
+@app.route('/cleanup', methods=['POST'])
+def deleteAllData():
+    for collection in db.list_collection_names():
+        db[collection].delete_many({})
+
+    return 'Data deleted', 204
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
